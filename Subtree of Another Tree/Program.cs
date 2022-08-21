6033b0ef333e296caa064717e364e7d3054d108c
jd_786 <jaydeep_shil@certree.com>
2022-08-21 17:26:59 +0530

    new test case

--- Subtree of Another Tree/Program.cs
+++ Subtree of Another Tree/Program.cs
@@ -7,7 +7,21 @@ namespace Subtree_of_Another_Tree
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      TreeNode root = new TreeNode(3);
+      root.left = new TreeNode(4);
+      root.right = new TreeNode(5);
+      root.left.left = new TreeNode(1);
+      root.left.right = new TreeNode(2);
+      root.left.right.left = new TreeNode(0);
+      root.left.right.right = new TreeNode(8);
+
+            TreeNode subRoot = new TreeNode(2);
+      subRoot.left = new TreeNode(0);
+      subRoot.right = new TreeNode(8);
+
+      Program p = new Program();
+      var answer = p.IsSubtree(root, subRoot);
+            Console.WriteLine(answer);
     }
     public class TreeNode
     {
